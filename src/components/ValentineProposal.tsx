dfa66dd57a76578a663278fb18137f75e0c8837e
erik-ksth <erikkizme@gmail.com>
2025-02-08 15:32:07 -0800

    update: change color

--- src/components/ValentineProposal.tsx
+++ src/components/ValentineProposal.tsx
@@ -93,6 +93,7 @@ const ValentineProposal = () => {
                                    damping: 20
                               }}
                               onHoverStart={moveNoButton}
+                              onClick={moveNoButton}
                               whileHover={{ scale: 1.05 }}
                               whileTap={{ scale: 0.95 }}
                          >
